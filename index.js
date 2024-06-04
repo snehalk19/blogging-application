fff21a8b88f055eb3ff3bffe3c7214fc10577e68
Snehal Khot <39563593+snehalk19@users.noreply.github.com>
2024-06-04 16:34:06 -0500

    Added dependency and div for blog

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 
 const cookieParser = require("cookie-parser");
 
+const Blog = require("./models/blog");
+
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 
 const {
   checkForAuthenticationCookie,
@@ -23,14 +26,18 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started at PORT: ${PORT}`);
